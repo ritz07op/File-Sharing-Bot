8bff43564aad1b46921b4081bca46f7956a88c63
ritz07op <137905088+ritz07op@users.noreply.github.com>
2025-01-10 16:37:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from bot import Bot
+import asyncio
 
-Bot().run()
+async def main():
+    bot = Bot()
+    await bot.start()  # Start the bot
+    await bot.idle()   # Keep it running
+
+if __name__ == "__main__":
+    asyncio.run(main())
